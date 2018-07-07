cb2e18c77d039cd98f4d7116acd2eeebb1d6c16b
stevenjmarsh <sjm.atl@gmail.com>
2018-07-07 17:22:14 -0400

    initial install of meteor and packages

---
+++ README.md
@@ -0,0 +1,21 @@
+# meteor_react_material
+
+A small, simple scaffold to start projects with Meteor, React, and Material-UI.
+
+## Install Components
+* Meteor
+* React
+* React Router
+* Eslint
+* Jest
+* Prettier
+* TODO
+  - Redux
+  - Redux Forms
+  - Storybook
+  - Flow (TBD)
+
+NOTE: check this project for other useful packages and setup steps.
+
+## Tech Notes
+* Roboto Font is included via link in main.html
---
+++ client/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Meteor } from "meteor/meteor";
+import { render } from "react-dom";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import { orange } from "@material-ui/core/colors";
+
+import App from "../imports/ui/App";
+
+const theme = createMuiTheme({
+  palette: {
+    // modify theme settings here.
+  },
+});
+
+Meteor.startup(() => {
+  render(
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>,
+    document.getElementById("render-target"),
+  );
+});
