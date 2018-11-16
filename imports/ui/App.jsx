f38d7839eacd4fb00d08a900ab526e50919c0b81
stevenjmarsh <sjm.atl@gmail.com>
2018-11-16 17:27:56 -0500

    moving AppBar to its own Header component

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,30 +1,16 @@
 /* eslint-disable arrow-parens, object-curly-newline */
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import { withStyles } from "@material-ui/core/styles";
-import styles from "./styles";
+import React, { Component, Fragment } from "react";
+import Header from "./components/Header";
 
 // App component - represents the whole app
-export default withStyles(styles)(
-  class App extends Component {
-    static propTypes = {
-      classes: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
-    };
+export default class App extends Component {
+  state = {};
 
-    state = {};
-
-    render() {
-      const { classes } = this.props;
-
-      return (
-        <Paper className={classes.root}>
-          <Typography variant="h1" align="center" gutterBottom>
-            Hello!
-          </Typography>
-        </Paper>
-      );
-    }
-  },
-);
+  render() {
+    return (
+      <Fragment>
+        <Header />
+      </Fragment>
+    );
+  }
+}
