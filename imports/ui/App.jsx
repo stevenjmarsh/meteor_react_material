899fffe806f9f06580bc2c9c7ac7ba81464adc0c
stevenjmarsh <sjm.atl@gmail.com>
2018-11-17 19:15:37 -0500

    adding Content component to App

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable arrow-parens, object-curly-newline */
 import React, { Component, Fragment } from "react";
 import Header from "./components/Header";
+import Content from "./components/Content";
 
 // App component - represents the whole app
 export default class App extends Component {
@@ -10,6 +11,7 @@ export default class App extends Component {
     return (
       <Fragment>
         <Header />
+        <Content />
       </Fragment>
     );
   }
